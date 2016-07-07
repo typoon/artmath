ef0929a157825351077c8cd7a83d95236ec5dde9
Henrique <typoon@gmail.com>
2016-07-07 18:43:42 -0400

    Added util functions for random numbers and creating 2D context

--- util.js
+++ util.js
@@ -3,3 +3,31 @@ function my_include(path) {
     s.src = path;
     document.body.appendChild(s);
 }
+
+function get2DContext() {
+    var canvas = document.createElement("canvas");
+
+    canvas.height = HEIGHT;
+    canvas.width = WIDTH;
+    canvas.style.border = "1px solid black";
+
+    var ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+
+    ctx.setPixel = function(x, y, w) {
+        this.fillRect(x, y, w, w);
+    }
+
+    return ctx;
+}
+
+
+function getRandom(max) {
+    return Math.random() * max;
+}
+
+function getRandomRound(max) {
+    return Math.round(Math.random() * max);
+}
+
+
